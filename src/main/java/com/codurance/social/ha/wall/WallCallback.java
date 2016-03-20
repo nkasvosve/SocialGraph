6c1f5d6b2ddfdcb3c14dc5da38a59ee8d03614a4
nkasvosve <nkasvosve@yahoo.com>
2016-03-20 05:00:24 +0000

    add new file

---
+++ src/main/java/com/codurance/social/ha/wall/WallCallback.java
@@ -0,0 +1,12 @@
+package com.codurance.social.ha.wall;
+
+import com.codurance.social.model.Posting;
+
+/**
+ * @author nickk
+ */
+
+public interface WallCallback {
+
+    void update(Posting posting);
+}
