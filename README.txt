b21a6b3c569011a4a61c4b097430d82d3f01b544
nkasvosve <nkasvosve@yahoo.com>
2016-03-23 05:58:22 +0000

    add some test cases

--- README.txt
+++ README.txt
@@ -1,7 +1,19 @@
 To run, type mvn clean install exec:java -Dexec.mainClass="com.codurance.social.app.SocialApplication" 
 
-Please note: Over the network functionality is disabled, though available
+The project uses Spring for dependency Injection and the Neo4J 
+
+database. As such, messages are available after application restart.
+
+Over the network functionality is disabled, though available
 
 via spring configuration of the classes in package 
 
-com.codurance.social.ha 
+com.codurance.social.ha (which uses Hazelcast). 
+
+This is an extension point that could be used 
+
+to make interaction feasible oer the network, and rather cheaply in 
+
+terms of effort. It would be necessary to make the model entities 
+
+properly serialisable for message exchange to work in Hazelcast.
--- src/test/java/com/codurance/social/app/CommandProcessorTest.java
+++ src/test/java/com/codurance/social/app/CommandProcessorTest.java
@@ -67,4 +67,44 @@ public class CommandProcessorTest {
 		Set<Posting> postings = user.getWall().getPostings();
 		org.junit.Assert.assertEquals(1, postings.size());
 	}
+	
+	@Test
+	public void showTimelineForUsers() {
+		User bob = new User();
+		bob.setUserName("JimBob");
+
+		User alice = new User();
+		alice.setUserName("Alice");
+
+		Mockito.stub(userRepository.findByUserName("JimBob")).toReturn(bob);
+		Mockito.stub(userRepository.findByUserName("Alice")).toReturn(alice);
+
+		processor.processInput("JimBob follows Alice");
+		processor.processInput("JimBob -> Hello there");
+		processor.processInput("JimBob -> I am hungry people");
+		processor.processInput("Alice -> What a day");
+
+		org.junit.Assert.assertEquals(2, bob.getPostings().size());
+		org.junit.Assert.assertEquals(1, alice.getPostings().size());
+	}
+	
+	@Test
+	public void showReadingOfUserMessage() {
+		User bob = new User();
+		bob.setUserName("JimBob");
+
+		User alice = new User();
+		alice.setUserName("Alice");
+
+		Mockito.stub(userRepository.findByUserName("JimBob")).toReturn(bob);
+		Mockito.stub(userRepository.findByUserName("Alice")).toReturn(alice);
+
+		processor.processInput("JimBob follows Alice");
+		processor.processInput("JimBob -> Hello there");
+		processor.processInput("JimBob -> I need lunch");
+		processor.processInput("Alice -> I am going home");
+
+		org.junit.Assert.assertEquals(2, bob.getPostings().size());
+		org.junit.Assert.assertEquals(1, alice.getPostings().size());				
+	}
 }
